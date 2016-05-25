17949c6262d8f53cdd7d4f8652d12f6eeedc562a
lbarreraabarca <lu.barreraabarca@gmail.com>
2016-05-25 20:02:29 -0300

    ip and port

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var server;
 /*
  * Create and start HTTP server.
  */
-
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 server = http.createServer(app);
-server.listen(process.env.PORT || 5000);
+server.listen(port, ip);
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
 });
