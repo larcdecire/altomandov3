c78b8c65d600a95df726f9cac995035cef343495
lbarreraabarca <lu.barreraabarca@gmail.com>
2016-06-20 18:31:08 -0300

    router use

--- controllers/api/secure/logs/index.js
+++ controllers/api/secure/logs/index.js
@@ -4,7 +4,8 @@ var auth = require('../../../../lib/auth');
 var UserModel = require('../../../../models/userclase13');
 
 module.exports = function (router) {
-
+    
+    //utiliza cualquier tipo de solicitud GET/POST/PUT/DELETE
     router.use('/', auth.isAuthenticated(), function (req, res, next) {
         next();
     });
