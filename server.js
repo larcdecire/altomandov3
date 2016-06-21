71617f9189d17bc1f9e361f8ccffb9e581efbeed
lbarreraabarca <lu.barreraabarca@gmail.com>
2016-06-20 22:25:50 -0300

    port 8080

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var server;
  * Create and start HTTP server.
  */
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 server = http.createServer(app);
 server.listen(port, ip);
 server.on('listening', function () {
