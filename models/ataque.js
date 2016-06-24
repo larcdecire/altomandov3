c39d39a6cb176ee4c36171bae7d818e377c9b42c
lbarreraabarca <lu.barreraabarca@gmail.com>
2016-06-24 13:17:14 -0300

    min, max ataque models

--- models/ataque.js
+++ models/ataque.js
@@ -7,7 +7,7 @@ var Schema = mongoose.Schema;
 var ataqueSchema = new Schema({
 
     codigo_escuadron: {type: String,enum:['SF', 'RT', 'HM'], trim: true, required: true},
-    p_exito:{type: Number, trim: true, required: true},
+    p_exito:{type: Number, trim: true, min:0, max: 100, required: true},
     cod_objMilitar: {type: String, trim: true, required: true}
 });
 
