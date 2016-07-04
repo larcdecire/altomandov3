24a30c996b957876c05588a1f4fd4ab3cad7b1d1
lbarreraabarca <lu.barreraabarca@gmail.com>
2016-07-04 14:24:22 -0400

    mensaje en ataque

--- models/ataque.js
+++ models/ataque.js
@@ -8,7 +8,8 @@ var ataqueSchema = new Schema({
 
     codigoEscuadron: {type: String,enum:['SF', 'RT', 'HM'], trim: true, required: true},
     pExito:{type: Number, trim: true, min:0, max: 100, required: true},
-    codObjMilitar: {type: String, trim: true, required: true}
+    codObjMilitar: {type: String, trim: true, required: true},
+    mensaje: {type: String, trim: true, required: true}
 });
 
 
